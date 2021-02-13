76d3539f07af6432204ffebd2d5f51790ba3f098
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2021-02-13 13:31:44 -0600

    Updated loggers

--- service-a/src/main/java/net/app/opentelemetry/service/a/ServiceAApplication.java
+++ service-a/src/main/java/net/app/opentelemetry/service/a/ServiceAApplication.java
@@ -1,18 +1,18 @@
 package net.app.opentelemetry.service.a;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
-
-		Logger logger = LogManager.getLogger();
-		logger.info("Welcome to 3rd session of JPG.");
+		log.info("Welcome to 3rd session of JPG.");
 	}
 
 }
--- service-b/src/main/java/net/app/opentelemetry/service/b/ControllerB.java
+++ service-b/src/main/java/net/app/opentelemetry/service/b/ControllerB.java
@@ -1,6 +1,7 @@
 package net.app.opentelemetry.service.b;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ControllerB {
 
   private final ServiceB serviceB;
 
   @GetMapping(value = "/service-b", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> controllerBMethod() {
+    log.info("Inside controllerBMethod...");
     return new ResponseEntity<>(serviceB.findAll(), HttpStatus.OK);
   }
 
