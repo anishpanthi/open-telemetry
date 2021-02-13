42acddbf91a504507a0981d1cb4f0e8b368cfc43
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2021-02-13 14:08:21 -0600

    Added @Log4j2

--- service-b/src/main/java/net/app/opentelemetry/service/b/ControllerB.java
+++ service-b/src/main/java/net/app/opentelemetry/service/b/ControllerB.java
@@ -1,6 +1,7 @@
 package net.app.opentelemetry.service.b;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@Slf4j
+@Log4j2
 public class ControllerB {
 
   private final ServiceB serviceB;
@@ -21,6 +22,7 @@ public class ControllerB {
   @GetMapping(value = "/service-b", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> controllerBMethod() {
     log.info("Inside controllerBMethod...");
+    log.debug("Inside controllerBMethod...");
     return new ResponseEntity<>(serviceB.findAll(), HttpStatus.OK);
   }
 
--- service-b/src/main/java/net/app/opentelemetry/service/b/ServiceB.java
+++ service-b/src/main/java/net/app/opentelemetry/service/b/ServiceB.java
@@ -3,6 +3,7 @@ package net.app.opentelemetry.service.b;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +12,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @RequiredArgsConstructor
-@Slf4j
+@Log4j2
 public class ServiceB {
 
   private final UserRepository userRepository;
 
   @SneakyThrows
   public List<UserEntity> findAll() {
-    Thread.sleep(1000);
+    Thread.sleep(500);
     List<UserEntity> userEntityList = userRepository.findAll();
     log.info("List of Users: {}", userEntityList);
     return userEntityList;
